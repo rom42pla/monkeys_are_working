1f2b513f392f87e9de3ee15d284add77765f445d
rom42pla <romeo.lanzino@gmail.com>
2022-12-15 14:58:27 +0100

    testing horse2zebra dataset

--- setup.py
+++ setup.py
@@ -1,3 +1,6 @@
+from glob import glob
+from os.path import splitext, basename
+
 import setuptools
 
 with open("README.md", "r", encoding="utf-8") as fp:
@@ -16,7 +19,8 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/rom42pla/monkeys_are_working",
     license="MIT",
-    packages=setuptools.find_packages("src/"),
-    package_dir={"": "src/"},
+    packages=setuptools.find_packages("monkeys_are_working/"),
+    package_dir={"": "monkeys_are_working/"},
+    # py_modules=[splitext(basename(path))[0] for path in glob("monkeys_are_working/*.py")],
     install_requires=requirements,
 )
