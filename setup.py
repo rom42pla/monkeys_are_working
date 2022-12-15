c9e08386e78e5277f93e26ee5d58b99e0c64ac1b
rom42pla <romeo.lanzino@gmail.com>
2022-12-15 14:12:50 +0100

    testing horse2zebra dataset

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("requirements.txt", "r", encoding="utf-8") as fp:
 
 setuptools.setup(
     name="monkeys_are_working",
-    version="0.0.1",
+    version="0.0.2",
     author="Romeo Lanzino",
     author_email="romeo.lanzino@gmail.com",
     description='Testing installation of Package',
