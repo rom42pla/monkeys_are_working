67ec4d8ed04830b56bf1befcfd8a8895b8b39248
rom42pla <romeo.lanzino@gmail.com>
2022-12-15 15:01:34 +0100

    testing horse2zebra dataset

--- setup.py
+++ setup.py
@@ -19,8 +19,8 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/rom42pla/monkeys_are_working",
     license="MIT",
-    packages=setuptools.find_packages("monkeys_are_working/"),
-    # package_dir={"": "monkeys_are_working/"},
+    packages=setuptools.find_packages(),
+    package_dir={"": "monkeys_are_working/"},
     # py_modules=[splitext(basename(path))[0] for path in glob("monkeys_are_working/*.py")],
     install_requires=requirements,
 )
