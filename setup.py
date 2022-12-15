77ab56505974f441da8f879480f76b2f8f8757be
rom42pla <romeo.lanzino@gmail.com>
2022-12-15 15:16:28 +0100

    testing horse2zebra dataset

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+import os
 from glob import glob
 from os.path import splitext, basename
 
@@ -6,8 +7,8 @@ import setuptools
 with open("README.md", "r", encoding="utf-8") as fp:
     long_description = fp.read()
 
-with open("requirements.txt", "r", encoding="utf-8") as fp:
-    requirements = [s for s in fp.read().split("\n") if s]
+# with open("requirements.txt", "r", encoding="utf-8") as fp:
+#     requirements = [s for s in fp.read().split("\n") if s]
 
 setuptools.setup(
     name="monkeys_are_working",
@@ -20,5 +21,16 @@ setuptools.setup(
     url="https://github.com/rom42pla/monkeys_are_working",
     license="MIT",
     packages=setuptools.find_packages(),
-    install_requires=requirements,
+    install_requires=[
+        "joblib==1.2.0",
+        "icecream==2.1.3",
+        "tqdm==4.64.1",
+        "numpy==1.23.5",
+        "einops==0.6.0",
+        "torch==1.13.0",
+        "torchvision==0.14.0",
+        "torchaudio==0.13.0",
+        "pandas==1.5.2",
+        "opencv-python==4.6.0.66",
+    ],
 )
